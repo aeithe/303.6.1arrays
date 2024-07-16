652c09d96efb2f406d3fba5959173ac4253f461e
pisareva.alisa@gmail.com <pisareva.alisa@gmail.com>
2024-07-16 08:26:49 -0400

    first and last commit

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+        // to see how IntelliJ IDEA suggests fixing it.
+        int[] age = {12, 4, 5, 2, 5};
+        System.out.println("Accessing Elements of an Array:");
+        System.out.println("First Element: " + age[0]);
+        System.out.println("Second Element: " + age[1]);
+        System.out.println("Third Element: " + age[2]);
+        System.out.println("Fourth Element: " + age[3]);
+        System.out.println("Fifth Element: " + age[4]);
+
+
+    }
+}
